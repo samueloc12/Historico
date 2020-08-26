ee4dfd2eaacc2850776c4fd16d8f1715834d481b
samueloc12 <70273180+samueloc12@users.noreply.github.com>
2020-08-26 08:30:21 -0500

    Add files via upload

---
+++ datos.php
@@ -0,0 +1,46 @@
+<DOCTYPE html>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	<div class="div">
+	  <center><h2>Bienvenido</h2></center>
+
+	<center><h2>Su nombre es: </h2></center>
+   
+</div>
+
+
+	</body>
+	</html>
+
+<?php
+$nombre=0;
+$nombre=$_POST['nombre' ];
+
+?>
+<!DOCTYPE html>
+<html>
+<center>
+	<br><br><br><br><br><br>
+</html>
+<?php
+echo $nombre;
+
+?>
+
+<html><br></html>
+
+
+ <style type="text/css">
+.div
+   {    	
+   	position: absolute;
+	height: 30%;
+	width: 35%;
+    background-color:;
+	left: 30%;
+    top: 2%;
+   }
---
+++ index.php
@@ -0,0 +1,37 @@
+<DOCTYPE html>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	<div class="div">
+	
+	  <form action="datos.php" method="POST">
+	  <center><h2>Formulario #1</h2></center>
+
+	 <center> <input type="text" name="nombre" placeholder ="Nombre"></center>
+	  <br>
+	  <center> <input type="submit" name="d2" value="Enviar"></input></center>
+	  <br>
+	   <center><input type="reset" name="d3" value ="Limpiar"></center>
+   
+</div>
+
+   </form>
+
+
+	</body>
+	</html>
+
+
+ <style type="text/css">
+.div
+   {    	
+   	position: absolute;
+	height: 30%;
+	width: 35%;
+    background-color:FFD296;
+	left: 30%;
+    top: 2%;
+   }
