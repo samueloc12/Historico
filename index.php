c658b61a19da61d5c8cf3372f79342e4d4f1117b
samueloc12 <70273180+samueloc12@users.noreply.github.com>
2020-08-28 11:39:17 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -4,21 +4,41 @@
 	<head>
 		<title></title>
 	</head>
-	<body>
-	<div class="div">
-	
-	  <form action="datos.php" method="POST">
-	  <center><h2>Formulario #1</h2></center>
+	<body bgcolor="C3C3C3">
 
-	 <center> <input type="text" name="nombre" placeholder ="Nombre"></center>
-	  <br>
-	  <center> <input type="submit" name="d2" value="Enviar"></input></center>
-	  <br>
-	   <center><input type="reset" name="d3" value ="Limpiar"></center>
+
+	<div class="banner"><center><h2><img src="baner.jpg"></h2></center></div>
+    <div class="botones"><center>
+    	<a href="index.php" class="B1"><center><img src="b1.jpg"></center></a>
+    <a href="fue.php" class="B2"><center><img src="b2.jpg"></center></a>
+<a href="est.php" class="B3"><center><img src="b3.jpg"></center></a>
+<a href="con.php" class="B4"><center><img src="b4.jpg"></center></a>
+</h2></center></div>
+    <div class="imagen"><center><h2>
+
+
+<img src="im4.jpg" class="im">
+    	
+<div class="d1">Durante esta pandemia, considerada la mas mortifera de toda la historia, se registraron aproximadamente 200 millones de muertes lo cual era el equivalente a el 65% de la poblacion europea, entre los años 1346 y 1353, contando con rebrotes que llegaron incluso al año 1450. Por lo tanto, los cuerpos no podian ser recogidos y enterrados, sino que eran tirados por la ciudad o al mar, esperando su descomposicion</div>
+
+	<div class="d2">Debido a la crisis, todo aquello externo al humano como lo eran los animales y los cultivos fueron descuidados, lo importante  era cuidarse de la muerte sin importar la perdida del ganado, por lo tanto, no solo se registraron millones de muertes humanas, sino que tambien murieron uchos animales debido al descuido de sus dueños. Tambien vale resaltar que en algunos animales como las ratas tambien se presentaba la peste negra y era extremadamente letal </div>
+
+		<div class="d3">Durante esta enfermedad algunas personas usaban artefactos como mascaras, sombreros o gafas que segun los medicos de la epoca purificaban el aire "podrido", que causaba la muerte. De alli es de donde viene la famosa mascara que simula el pico de un ave</div>
+
+			<div class="d4">Al ser tantas las muertes, las personas veian a la peste negra como un castigo de Dios que se asemejaba al fin del mundo donde los muertos irian por los vivos. Por dicha razon, podemos ver que en muchas de las pinturas que representaban la pandemia como los cadaveres van tras las personas vivas intentando acabar con el resto de la poblacion que queda.</div>
+			
+				<div class="d5"></div>
+					<div class="d6"></div>
+						<div class="d7"></div>
+
+    </div>
+
+
+
+
+    <div class="pie"><center><h2></h2></center></div><div class="pie2"><center><h2></h2></center></div>
    
 </div>
-
-   </form>
 
 
 	</body>
@@ -26,12 +46,289 @@
 
 
  <style type="text/css">
-.div
+
+.banner
    {    	
    	position: absolute;
-	height: 30%;
-	width: 35%;
-    background-color:FFD296;
-	left: 30%;
+	height: 15%;
+	width: 96%;
+    background-color:F0F3F1;
+	left: 2%;
     top: 2%;
+    border:solid black 3px;
    }
+
+   .botones
+   {    	
+   	position: absolute;
+	height: 5.1%;
+	width: 96%;
+    background-color:7C7C7C;
+	left: 2%;
+    top: 19%;
+    
+   }
+
+   .imagen
+   {    	
+   	position: absolute;
+	height: 65%;
+	width: 96%;
+    background-color:C0C0C0;
+	left: 2%;
+    top: 26%;
+    border:solid black 3px;
+   }
+
+    .im
+   {    	
+   	position:absolute;
+	height: 98%;
+	width: 99%;
+	left: 0.5%;
+    top: 1%;
+    size:1%;
+   }
+
+
+   .pie
+   {    	
+   	position: absolute;
+	height: 5%;
+	width: 60%;
+    background-color:F0F3F1;
+	left: 2%;
+    top: 93%;
+    border:solid black 3px;
+   }
+
+   .pie2
+   {    	
+   	position: absolute;
+	height: 5%;
+	width: 34%;
+    background-color:F0F3F1;
+	left: 64%;
+    top: 93%;
+    border:solid black 3px;
+   }
+
+
+  
+   .B1
+   {    	
+   	position: absolute;
+	height: 94%;
+	width: 25%;
+    background-color:F0F3F1;
+	left: 0%;
+    top: 3%;
+    text-decoration: none;
+   border:solid black 3px;
+   }
+
+   .B2
+   {    	
+   	position: absolute;
+	height: 94%;
+	width: 25%;
+    background-color:F0F3F1;
+	left: 25%;
+    top: 3%;
+    text-decoration: none;
+   border:solid black 3px;
+   }
+
+
+   .B3
+   {    	
+   	position: absolute;
+	height: 94%;
+	width: 25%;
+    background-color:F0F3F1;
+	left: 50%;
+    top: 3%;
+    text-decoration: none;
+   border:solid black 3px;
+   }
+
+
+   .B4
+   {    	
+   	position: absolute;
+	height: 94%;
+	width: 25%;
+    background-color:F0F3F1;
+	left: 75%;
+    top: 3%;
+    text-decoration: none;
+   border:solid black 3px;
+   }
+
+
+.d1
+{
+		position: absolute;
+	left: 1%;
+	top: 27%;
+	height:43%;
+	width: 15%;
+	background:rgba(211,61,21,0);
+	font-size: 15px;
+	font-size:0;
+}
+
+
+.d1:hover
+{
+		position: absolute;
+	left: 1%;
+	top: 27%;
+	height:45%;
+	width: 20%;
+	background-color:white;
+	border:solid black 3px;
+	font-size:15;
+}
+
+
+.d2
+{
+		position: absolute;
+	left: 25%;
+	top: 20%;
+	height:20%;
+	width: 25%;
+	background:rgba(211,61,21,0);
+	font-size:0;
+}
+
+
+.d2:hover
+{
+		position: absolute;
+	left: 25%;
+	top: 20%;
+	height:40%;
+	width: 25%;
+	background-color:white;
+	border:solid black 3px;
+	font-size:15;
+}
+
+
+.d3
+{
+		position: absolute;
+	left: 28%;
+	top: 75%;
+	height:20%;
+	width: 10%;
+	background:rgba(211,61,21,0);
+	font-size:0;
+}
+
+
+.d3:hover
+{
+		position: absolute;
+	left: 28%;
+	top: 75%;
+	height:20%;
+	width: 30%;
+	background-color:white;
+	border:solid black 3px;
+	font-size:15;
+}
+
+.d4
+{
+		position: absolute;
+	left: 45%;
+	top: 1%;
+	height:14%;
+	width: 12%;
+	background:rgba(211,61,21,0);
+	font-size:0;
+}
+
+
+.d4:hover
+{
+		position: absolute;
+	left: 45%;
+	top: 1%;
+	height:30%;
+	width: 20%;
+	background-color:white;
+	border:solid black 3px;
+	font-size:15;
+}
+
+.d5
+{
+		position: absolute;
+	left: 78%;
+	top: 10%;
+	height:35%;
+	width: 15%;
+	background:rgba(211,61,21,0);
+	font-size:0;
+}
+
+
+.d5:hover
+{
+		position: absolute;
+	left: 78%;
+	top: 10%;
+	height:35%;
+	width: 15%;
+	background-color:white;
+	border:solid black 3px;
+	font-size:15;
+}
+
+.d6
+{
+		position: absolute;
+	left: 96%;
+	top: 20%;
+	height:43%;
+	width: 4%;
+	background:rgba(211,61,21,0);
+}
+
+
+.d6:hover
+{
+		position: absolute;
+		left: 96%;
+	top: 20%;
+	height:43%;
+	width: 4%;
+	background-color:white;
+	border:solid black 3px;
+}
+
+.d7
+{
+		position: absolute;
+	left: 85%;
+	top: 70%;
+	height:30%;
+	width: 15%;
+	background:rgba(211,61,21,0);
+}
+
+
+.d7:hover
+{
+		position: absolute;
+	left: 85%;
+	top: 70%;
+	height:30%;
+	width: 15%;
+	background-color:white;
+	border:solid black 3px;
+}
